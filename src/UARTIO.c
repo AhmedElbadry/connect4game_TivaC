46a7900515e62a4229cc4f710079702017e93f2d
AhmedElBadry <ahmed2lbadry@gmail.com>
2018-06-01 11:50:26 +0200

    added grid images

---
+++ src/UARTIO.c
@@ -0,0 +1,114 @@
+// UART.c
+// Runs on LM3S811, LM3S1968, LM3S8962, LM4F120
+// Simple device driver for the UART.
+// Daniel Valvano
+// June 17, 2013
+
+/* This example accompanies the books
+  "Embedded Systems: Introduction to ARM Cortex M Microcontrollers",
+  ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2013
+
+"Embedded Systems: Real Time Interfacing to ARM Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2013
+ 
+ Copyright 2013 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+
+// U0Rx (VCP receive) connected to PA0
+// U0Tx (VCP transmit) connected to PA1
+
+#include "UARTIO.h"
+#include <stdio.h>
+
+#define GPIO_PORTC_AFSEL_R      (*((volatile unsigned long *)0x40006420))
+#define GPIO_PORTC_DEN_R        (*((volatile unsigned long *)0x4000651C))
+#define GPIO_PORTC_AMSEL_R      (*((volatile unsigned long *)0x40006528))
+#define GPIO_PORTC_PCTL_R       (*((volatile unsigned long *)0x4000652C))
+#define UART1_DR_R              (*((volatile unsigned long *)0x4000D000))
+#define UART1_FR_R              (*((volatile unsigned long *)0x4000D018))
+#define UART1_IBRD_R            (*((volatile unsigned long *)0x4000D024))
+#define UART1_FBRD_R            (*((volatile unsigned long *)0x4000D028))
+#define UART1_LCRH_R            (*((volatile unsigned long *)0x4000D02C))
+#define UART1_CTL_R             (*((volatile unsigned long *)0x4000D030))
+#define UART_FR_TXFF            0x00000020  // UART Transmit FIFO Full
+#define UART_FR_RXFE            0x00000010  // UART Receive FIFO Empty
+#define UART_LCRH_WLEN_8        0x00000060  // 8 bit word length
+#define UART_LCRH_FEN           0x00000010  // UART Enable FIFOs
+#define UART_CTL_UARTEN         0x00000001  // UART Enable
+#define SYSCTL_RCGC1_R          (*((volatile unsigned long *)0x400FE104))
+#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
+#define SYSCTL_RCGC1_UART1      0x00000002  // UART1 Clock Gating Control
+#define SYSCTL_RCGC2_GPIOC      0x00000004  // port C Clock Gating Control
+
+//------------UART_Init------------
+// Initialize the UART for 115,200 baud rate (assuming 80 MHz UART clock),
+// 8 bit word length, no parity bits, one stop bit, FIFOs enabled
+// Input: none
+// Output: none
+void UART_Init(void){
+  SYSCTL_RCGC1_R |= SYSCTL_RCGC1_UART1; // activate UART1
+  SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOC; // activate port C
+  UART1_CTL_R &= ~UART_CTL_UARTEN;      // disable UART
+  UART1_IBRD_R = 43;  // 80,000,000/(16*115,200)) = 43.40278
+  UART1_FBRD_R = 26;  //6-bbit fraction, round(0.40278 * 64) = 26
+                                        // 8 bit word length (no parity bits, one stop bit, FIFOs)
+  UART1_LCRH_R = (UART_LCRH_WLEN_8|UART_LCRH_FEN);
+  UART1_CTL_R |= UART_CTL_UARTEN;       // enable UART
+  GPIO_PORTC_AFSEL_R |= 0x30;           // enable alt funct on PC5-4
+  GPIO_PORTC_DEN_R |= 0x30;             // enable digital I/O on PC5-4
+                                        // configure PC5-4 as UART1
+  GPIO_PORTC_PCTL_R = (GPIO_PORTC_PCTL_R&0xFF00FFFF)+0x00220000;
+  GPIO_PORTC_AMSEL_R &= ~0x30;          // disable analog functionality on PC5-4
+}
+
+//------------UART_InChar------------
+// Wait for new serial port input
+// Input: none
+// Output: ASCII code for key typed
+unsigned char UART_InChar(void){
+  while((UART1_FR_R&UART_FR_RXFE) != 0);
+  return((unsigned char)(UART1_DR_R&0xFF));
+}
+//------------UART_OutChar------------
+// Output 8-bit to serial port
+// Input: letter is an 8-bit ASCII character to be transferred
+// Output: none
+void UART_OutChar(unsigned char data){
+  while((UART1_FR_R&UART_FR_TXFF) != 0);
+  UART1_DR_R = data;
+}
+
+
+
+// Print a character to OLED.
+int fputc(int ch, FILE *f){
+  if((ch == 10) || (ch == 13) || (ch == 27)){
+    UART_OutChar(13);
+    UART_OutChar(10);
+    return 1;
+  }
+  UART_OutChar(ch);
+  return 1;
+}
+// No input from UART, always return data.
+int fgetc (FILE *f){
+	char ch;
+	ch = UART_InChar();
+	UART_OutChar(ch);
+  return ch;
+}
+// Function called when file error occurs.
+int ferror(FILE *f){
+  /* Your implementation of ferror */
+  return EOF;
+}
+
---
+++ src/uartio.d
@@ -0,0 +1,3 @@
+.\uartio.o: UARTIO.c
+.\uartio.o: UARTIO.h
+.\uartio.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
